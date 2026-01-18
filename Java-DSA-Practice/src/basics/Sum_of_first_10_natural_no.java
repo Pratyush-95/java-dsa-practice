790ea0824904dcf51df3c8442c784af2b31e10ae
Pratyush Kumar <134200485+Pratyush-95@users.noreply.github.com>
2026-01-18 23:50:28 +0530

    Initial commit: Java DSA practice with basic programs

---
+++ Java-DSA-Practice/src/basics/Sum_of_first_10_natural_no.java
@@ -0,0 +1,18 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Sum_of_first_10_natural_no {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for(int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+
+        System.out.print(sum);
+    }
+}
