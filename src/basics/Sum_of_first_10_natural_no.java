e3ac989353ac1969f6ea31ba01407d0ec1e9a4de
Pratyush Kumar <134200485+Pratyush-95@users.noreply.github.com>
2026-01-19 00:09:33 +0530

    Add basic Java DSA programs (primes & natural numbers)

---
+++ src/basics/Sum_of_first_10_natural_no.java
@@ -0,0 +1,11 @@
+package basics;
+import java.util.Scanner;
+public class Sum_of_first_10_natural_no {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+	}
+
+}
