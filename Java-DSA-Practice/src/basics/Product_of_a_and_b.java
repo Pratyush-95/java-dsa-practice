4917f357bd1963ca7950f6e2f237e2d99fa129ca
Pratyush Kumar <134200485+Pratyush-95@users.noreply.github.com>
2026-01-19 14:26:13 +0530

    Add basic Java programs (Fibonacci, circle area, product)

---
+++ Java-DSA-Practice/src/basics/Product_of_a_and_b.java
@@ -0,0 +1,14 @@
+package basics;
+import java.util.Scanner;
+public class Product_of_a_and_b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int product=a*b;
+		System.out.print(product);
+	}
+
+}
